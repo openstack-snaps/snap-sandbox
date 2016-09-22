a7925635ecaac969265e2477614807bb68a618c3
Sam Yaple <sam@yaple.net>
2016-09-22 14:43:27 +0000

    Switch to non-deprecated 'dump' module
    This also requires rearranging the configuration file around a bit.
    
    Adding in uwsgi and apache2 configs as well.

--- keystone/conf/bin/run.sh
+++ keystone/conf/bin/run.sh
@@ -7,4 +7,9 @@ if [ $(id -u) -eq 0 ]; then
         $SNAP/templates/keystone-snap.conf > $SNAP_COMMON/keystone.conf.d/keystone-snap.conf
 fi
 
-exec "$SNAP/usr/bin/python2" "$SNAP/usr/bin/$1" --config-file=$SNAP/etc/keystone/keystone.conf --config-dir=$SNAP_COMMON/keystone.conf.d "${@:2}"
+/usr/bin/env uwsgi --master --emperor /etc/uwsgi &
+
+source /etc/apache2/envvars
+export APACHE_DIR="apache2"
+/usr/sbin/apache2 -DFOREGROUND
+#/usr/bin/env python "$SNAP/usr/bin/$1" --config-file=$SNAP/etc/keystone/keystone.conf "${@:2}"
